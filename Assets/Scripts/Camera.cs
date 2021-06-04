59bb5294c999e5fa630eb75e808f0501cf1cb10e
Hubert Walton <hewalton@g.coastal.edu>
2021-06-04 02:48:56 -0400

    modified camera/player movement

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -3,32 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class Camera : MonoBehaviour
+public class Camera : NetworkBehaviour
 {
     Camera main;
     Player player;
     Rigidbody body;
+    Collider c;
     Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        main = GetComponent<Camera>();
-        player = GetComponentInParent<Player>();
+        main = GetComponentInChildren<Camera>();
+        player = GetComponent<Player>();
+        // main = GetComponent<Camera>();
+        // player = GetComponentInParent<Player>();
+        c = GetComponentInChildren<Collider>();
         
         if(player == null)
             Debug.Log("Null player");
 
         body = player.GetComponentInChildren<Rigidbody>();
 
-        offset = transform.position - body.transform.position;
+        // offset = main.transform.position - body.transform.position;
+        offset = main.transform.position - c.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if(!isLocalPlayer)
-        //     return;
+        if(!isLocalPlayer)
+            return;
 
-        transform.position =  body.transform.position + offset;
+        // main.transform.position =  body.transform.position + offset;
+        main.transform.position =  c.transform.position + offset;
     }
 }
